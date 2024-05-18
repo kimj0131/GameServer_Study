529f29a351b101fab7915f38a0e5b524e68688ae
junhyoungkim <kkjn0131@naver.com>
2024-05-18 21:16:02 +0900

    * 컴파일러 최적화
    - C# 에서는 디버그 모드와 릴리즈 모드의 컴파일러 동작이 다르다
     > 릴리즈 모드에서는 코드의 최적화 작업이 일어남
    - volatile를 사용하면 최적화작업을 제외할 수 있으나 잘 사용하지는 않는다

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,58 +2,39 @@
 {
     internal class Program
     {
-        static void MainThread(object state)
+        // 전역변수는 모든 Thread가 동시에 접근할 수 있다
+        // volatile : 휘발성데이터 선언(릴리즈 모드 컴파일시 최적화 하지않음)
+        // C++와 동작이 다르기 때문에 혼동우려가 있어 잘 사용하지 않는다고 함
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("쓰레드 시작!");
+
+            while (_stop == false)
             {
-                Console.WriteLine("Hello Thread!");
+                // 누군가가 stop 신호를 해주기를 기다린다
             }
+
+            Console.WriteLine("쓰레드 종료!");
         }
+
         static void Main(string[] args)
         {
-            // 쓰레드 생성 및 함수 연결 / 스레드는 기본적으로 foreground쓰레드로 만들어짐
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            // 백그라운드 쓰레드로 지정할 수 있음
-            // Main이 종료되면 쓰레드가 실행중인 여부와 상관없이 종료된다
-            //t.IsBackground = true;
-            //t.Start();
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            //Console.WriteLine("Waitng for Thread!");
+            // 1000밀리세컨드 만큼 대기를 한다
+            // 1초의 대기시간 후에 _stop을 true로 전환
+            Thread.Sleep(1000);
+            _stop = true;
 
-            // Join을 하면 백그라운드 쓰레드가 종료될때 까지 기다린다.
-            //t.Join();
-            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
 
-            // C#에서 지원하는 쓰레드풀링을 활용 >> 백그라운드에서 돌아가는것으로 추정
-            // 직접 생성해서 쓰기보다 쓰레드풀링을 활용하는 것이 좋을 것
-            // 최대로 동시에 돌릴수 있는 쓰레드를 자동으로 제한하기 때문에 직접 생성하는것에 대비 
-            //ThreadPool.QueueUserWorkItem(MainThread);
+            t.Wait();
 
-            // SetMinThreads(최소 스레드, ?)
-            ThreadPool.SetMinThreads(1, 1);
-            // SetMinThreads(최대 스레드, ?)
-            ThreadPool.SetMaxThreads(5, 5);
-
-            //for (int i = 0; i < 4; i++)
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) ; });
-
-            // task를 이용하면 작업단위로 스케줄링
-            // TaskCreationOptions.LongRunning : 다른 스레드 작업과는 다르게 오래걸리는 작업이 될것임을 알려줌 (워크 쓰레드 풀과 별도로 처리해준다)
-            // 오래 사용할 것이지에 따라서 쓰레드풀을 조금더 효율적으로 관리할 수 있다
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) ; });
-                t.Start();
-            }
-
-
-            ThreadPool.QueueUserWorkItem(MainThread);
-            while (true)
-            {
-
-            }
-
+            Console.WriteLine("종료 성공");
         }
     }
 }
