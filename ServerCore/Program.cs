9a9f64ed3ee0d46147cbee7fc67ea2be0e94d705
junhyoungkim <kkjn0131@naver.com>
2024-05-19 21:43:28 +0900

    * 경합조건과 Interlocked

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,64 +2,53 @@
 {
     internal class Program
     {
-        // ** 별도의 조치없이 컴파일시 CPU가 성능을 좋게 하기위해 최적화를 거쳐 순서에 맞지 않게 실행시키는 결과가 나오므로
-        // ** 예상했던 무한루프결과가 아닌 반복을 빠져나오는 결과가 나오게된다.
-        // ** 이런현상을 해결하고 코드의 흐름을 의도한대로 조절하기위해 메모리 베리어를 사용한다
+        // 경합조건 (Race Condition) : 두 개 이상의 프로세스가 공통 자원을 병행적으로 읽거나 쓰는 동작을 할 때,
+        // 공용 데이터에 대한 접근이 어떤 순서에 따라 이루어졌는지에 따라 그 실행 결과가 같지않고 달라지는 상황
 
-        // 메모리 베리어
-        // A) 코드 재배치 억제
-        // B) 가시성도 해결가능
-
-        // 종류
-        // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다.
-        // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
-        // 2) Load Memory Barrier (ASM LFENCE) : Load만 막는다
-
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1; // Store y 메모리에 적재
+            // atomic = 원자성
+            // 어떠한 동작은 한번에 일어나야 한다
 
-            Thread.MemoryBarrier();
+            for (int i = 0; i < 100000; i++)
+            {
+                //number++;
 
-            r1 = x; // Load x 메모리에서 가져옴
+                // 3단계를 걸쳐 연산이 진행되기 때문에 문제가 발생
+                //int temp = number;
+                //temp += 1;
+                //number = temp;
+
+                // Interlocked > 원자성을 보장하게 한다
+                // 성능에서는 매우 큰 손해를 보게된다
+                Interlocked.Increment(ref number);
+            }
+
         }
 
         static void Thread_2()
         {
-            x = 1; // Store x
+            for (int i = 0; i < 100000; i++)
+            {
+                //number--;
 
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+                Interlocked.Decrement(ref number);
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
+            t1.Start();
+            t2.Start();
 
-                Task.WaitAll(t1, t2);
+            Task.WaitAll(t1, t2);
 
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-
-            // 
-
-            Console.WriteLine($"{count}번만에 빠져나옴");
+            Console.WriteLine(number);
         }
     }
 }
