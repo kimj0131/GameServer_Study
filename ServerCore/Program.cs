4ba8be8bcd1c632ef52cab2d750d9d898985e0dc
junhyoungkim <kkjn0131@naver.com>
2024-05-28 08:30:24 +0900

    * Auto Reset Event

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,39 +1,22 @@
 ﻿namespace ServerCore
 {
-    class SpinLock
+    class Lock
     {
-        // 상태변수
-        volatile int _locked = 0;
+        // true 열린상태 false 닫힌상태
+        // 커널단에서는 bool과 비슷한 역할을 한다
+        AutoResetEvent _available = new AutoResetEvent(true);
 
         // 획득
         public void Acquire()
         {
-            // 잠기고 풀리는 과정이 한번에 일어나야함
-            while (true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-
-                // 다른 일반적인 버전
-                //CAS Compare-And-Swap 연산
-                int expected = 0;   // 예상값
-                int desired = 1;    // 원하는값
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-
-                // 반복문을 계속 돌지 않고 휴식
-                // Thread.Sleep(1); // 무조건 휴식 
-                // Thread.Sleep(0); // 조건부 양보 > 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
-                Thread.Yield();  // 관대한 양보 > 실행이 가능한 쓰레드가 있으면 그 쓰레드가 실행 > 실행 가능한 쓰레드가 없으면 남은시간 소진
-
-            }
+            // 입장 시도
+            _available.WaitOne();
         }
 
         // 반환
         public void Release()
         {
-            _locked = 0;
+            _available.Set();
         }
     }
 
@@ -41,7 +24,7 @@
     {
         static int _num = 0;
 
-        static SpinLock _lock = new SpinLock();
+        static Lock _lock = new Lock();
 
         static void Thread_1()
         {
