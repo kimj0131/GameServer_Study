8b2d7060dc601bfe7b1f65bf99dd9d9150a07793
junhyoungkim <kkjn0131@naver.com>
2024-05-28 19:30:10 +0900

    * ReaderWriteLock 재귀적 락을 허용하는 경우  - 실행결과

--- ServerCore/Lock.cs
+++ ServerCore/Lock.cs
@@ -2,7 +2,7 @@
 {
     // ReaderWriteLock 구현해보기
 
-    // 재귀적 락을 허용할 것인지 (No)
+    // 재귀적 락을 허용할 것인지 (Yes) WriteLock -> WriteLock OK, WriteLock -> ReadLock OK, ReadLock -> WriteLock NO
     // 스핀락 정책 (5000번 -> Yield)
     internal class Lock
     {
@@ -15,9 +15,20 @@
         // WriteThreadId > WriteLock을 획득한 Thread의 ID
         // ReadCount > ReadLock을 획득했을때 여러 쓰레드들이 동시에 잡았을때 의 카운트
         int _flag = EMPTY_FLAG;
+        // 상호 베타적 관계이므로 플래그에 넣을 필요가 없다
+        // 누군가가 writelock을 잡은경우 해당 쓰레드만 접근이 가능하기 때문
+        int _writeCount = 0;
 
         public void WriteLock()
         {
+            // 동일 쓰레드가 WriteLock을 이미 획득하고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                _writeCount++;
+                return;
+            }
+
             // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다.
             int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
             while (true)
@@ -26,18 +37,31 @@
                 {
                     // 시도를 해서 성공하면 return
                     if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
                         return;
+                    }
                 }
                 Thread.Yield();
             }
         }
         public void WriteUnlock()
         {
-            Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+            int lockCount = --_writeCount;
+            if (lockCount == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
         }
 
         public void ReadLock()
         {
+            // 동일 쓰레드가 WriteLock을 이미 획득하고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
             // 아무도 WriteLock을 획득하고 있지 않으면, ReadCount를 1 늘린다
             while (true)
             {
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
+
         static void Main(string[] args)
         {
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.ReadLock();
+                    count++;
+                    _lock.ReadUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.ReadLock();
+                    count--;
+                    _lock.ReadUnlock();
+                }
+            });
 
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
         }
     }
 }
