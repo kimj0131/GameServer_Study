86c6d75937bde2e395e50dab6dcf93a810ae130a
junhyoungkim <kkjn0131@naver.com>
2024-05-28 19:05:19 +0900

    * ReaderWriteLock 구현해보기 1

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,42 +2,9 @@
 {
     internal class Program
     {
-        static int _num = 0;
-        // mutex 커널동기화 객체
-        // bool, int, ThreadId 
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
 
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
         }
     }
 }
