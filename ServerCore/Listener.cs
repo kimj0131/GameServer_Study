34309c6de340b3744f584205c7029825038e310d
junhyoungkim <kkjn0131@naver.com>
2024-06-04 15:50:33 +0900

    * Listener  - accept를 비동기방식 (Non-blocking)으로 구현

---
+++ ServerCore/Listener.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore
+{
+    internal class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            // 문지기 교육 ipAddress 연동
+            _listenSocket.Bind(endPoint);
+
+            // 영업시작 Listen
+            // backlog : 최대 대기수
+            _listenSocket.Listen(10);
+
+            // SocketAsyncEventArgs는 한번 생성하면 계속 재사용할 수 있다
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+
+            // 최초 한번은 직접등록하여 접속자를 기다린다
+            RegisterAccept(args);
+            // 클라이언트가 실제로 커넥트 요청이 오면 콜백방식으로 OnAcceptCompleted가 호출이 된다
+        }
+
+        // 접속 예약?
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            // SocketAsyncEventArgs를 비워줘야 다음 접속자의 정보를 불러올 수 있음
+            args.AcceptSocket = null;
+
+            // 계류 중인지를 반환한다 >> false일때 접속이 되었다는 의미?
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        // Accept가 성공했을때 실행되는 로직
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                // TODO
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            // 접속을 위한 일이 끝났으므로 다음 접속자를 위해 등록
+            RegisterAccept(args);
+        }
+
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,44 +1,56 @@
-﻿namespace ServerCore
-{
-    // [ JobQueue ] 
-    // 모든쓰레드들이 접근할 때 락을 이용해서 풀었다 잠궜다 하기보다는 
-    // TLS공간에 여러개의 일감을 뽑아와 활용하면 된다
-    // 대량의 일감을 처리하기 전에 JobQueue에 접근할 필요성이 없어진다
-    // 공용 공간에 접근하는 횟수를 줄이는데 의미가 있다고 할 수 있다
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
+namespace ServerCore
+{
     internal class Program
     {
-        // TLS 실습
+        static Listener _listener = new Listener();
 
-        // static 전역메모리, 모든 Thread가 공유해서 사용한다
-        // TLS 영역으로 두고 싶다면 ThreadLocal로 wrapping한다
-        // Thread 마다 접근하면 자신의 공간에 저장이되기 때문에 특정 Thread에서 수정을 하더라도 다른 Thread에서는 영향을 주지 않는다
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
-        //static string ThreadName;
-
-        static void WhoAmI()
+        static void OnAcceptHandler(Socket clientSocket)
         {
-            //ThreadName.Value = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
-            //ThreadName = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
+            try
+            {
+                // 받는다 
+                byte[] recvBuff = new byte[1024];   // 받은 데이터를 저장하는 공간?
+                int recvByte = clientSocket.Receive(recvBuff);     // 몇 byte를 받았는지 반환한다
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvByte);
+                Console.WriteLine($"[From Client] {recvData}");
 
-            bool repeat = ThreadName.IsValueCreated;
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+                clientSocket.Send(sendBuff);
 
-            if (repeat)
-                Console.WriteLine(ThreadName.Value + "Repeat");
-            else
-                Console.WriteLine(ThreadName.Value);
-
-            //Console.WriteLine(ThreadName);
+                // 손님을 내보낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry iPHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = iPHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 필요없어질 경우 Dispose를 이용해 제거한다
-            ThreadName.Dispose();
+            // 문지기 listenSocket
+
+            // 손님을 입장시킨다 > Init에서 OnAcceptCompleted이벤트를 통해 접속한다
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                ;
+            }
+
         }
     }
 }
